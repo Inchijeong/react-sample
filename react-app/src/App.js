e9a50cd86efe46a2417197d07198ee1d5b2a7e8b
Inchijeong <cde0409@naver.com>
2019-10-25 22:49:08 +0900

    Add an event step 3

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      mode:'welcome',
+      mode:'read',
       subject:{title:'WEB', sub:'World wide web!'},
       welcome:{title:'Welcome', desc:'Hello, React!'},
       contents:[
@@ -38,8 +38,11 @@ class App extends Component {
             <h1><a href="/" onClick={function(e){
               console.log(e);
               e.preventDefault();
-              alert('hi');
-            }}>{this.state.subject.title}</a></h1>
+              // this.state.mode = 'welcome';
+              this.setState({
+                mode:'welcome'
+              });
+            }.bind(this)}>{this.state.subject.title}</a></h1>
             {this.state.subject.sub}
         </header>
         <TOC data={this.state.contents}></TOC>
