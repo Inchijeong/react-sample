dd07e3854d56fdb62e918bae5c5f19c88cbc67b0
Inchijeong <cde0409@naver.com>
2019-10-25 23:47:28 +0900

    Add an event step 4

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -28,23 +28,17 @@ class App extends Component {
       _title = this.state.contents[0].title;
       _desc = this.state.contents[0].desc;
     }
+    console.log('render', this);
     return (
       <div className="App">
-        {/* <Subject
+        <Subject
           title={this.state.subject.title}
-          sub={this.state.subject.sub}>
-        </Subject> */}
-        <header>
-            <h1><a href="/" onClick={function(e){
-              console.log(e);
-              e.preventDefault();
-              // this.state.mode = 'welcome';
-              this.setState({
-                mode:'welcome'
-              });
-            }.bind(this)}>{this.state.subject.title}</a></h1>
-            {this.state.subject.sub}
-        </header>
+          sub={this.state.subject.sub}
+          onChangePage={function(){
+            this.setState({mode:'welcome'});
+          }.bind(this)}
+        >
+        </Subject>
         <TOC data={this.state.contents}></TOC>
         <Content title={_title} desc={_desc}></Content>
       </div>
